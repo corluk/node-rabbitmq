a9866298dccabbe7393f80979a216fdcdf376da6
osman corluk <corluk@hotmail.com>
2023-01-11 23:49:03 +0300

    d

--- app/workerqueue/consumer.ts
+++ app/workerqueue/consumer.ts
@@ -1,26 +1,44 @@
 import amqp , {Channel, Connection, Message}from "amqplib"
 
-const RABBITMQ_URI = "amqp://user1:iuS9NBmVPx7ThqW@localhost:5672/";
+const RABBITMQ_URI = "amqp://ozm_admin:29c22461b8d8e3582319cbc04c05ad98@localhost:5672/";
 
+
+interface IMessage {
+    message : string 
+}
 (async()=>{
     const con = await amqp.connect(RABBITMQ_URI)
  
     const chan = await con.createChannel() 
-    let queue = "hello"
+    let queue = "task_queue"
     let msg = {
             message : "Hello World"
         }
     chan.assertQueue(queue, {
-        durable : false
+        durable : true 
     })
 
     chan.consume(queue , (message)=>{
-        console.log(`[x] Received ${message?.content.toString()}`)
+       
 
-        chan.ack(message as Message)
+        if (message?.content ){
+            const b = message?.content || JSON.stringify({})
+            let content = JSON.parse(b.toString()) as {}  as IMessage
+            console.log(`content is ${content.message}`)
+            console.debug(content)
+            if ( content.message ){
+                let sec = content.message.split(".").length -1 
+    
+                console.log(`[x] Received ${content.toString()}`)
+                setTimeout(()=>{}, sec  * 1000 )
+            }
+        }
+        
+
+      ///  chan.ack(message as Message)
 //        console.log(`[x] Received ${JSON.stringify(msg?.content.toJSON())}`)
     },{
-        noAck : true 
+        noAck : true  
     })
      
    
--- app/workerqueue/producer.ts
+++ app/workerqueue/producer.ts
@@ -1,21 +1,23 @@
 import amqp , {Channel, Connection}from "amqplib"
 
-const RABBITMQ_URI = "amqp://user1:iuS9NBmVPx7ThqW@localhost:5672/";
+const RABBITMQ_URI = "amqp://ozm_admin:29c22461b8d8e3582319cbc04c05ad98@localhost:5672/";
 
 (async()=>{
     const con = await amqp.connect(RABBITMQ_URI)
  
     const chan = await con.createChannel() 
-    let queue = "hello"
+    let queue = "task_queue"
     let msg = {
-            message : "Hello World"
+            message : process.argv.slice(2).join(" ") || "Hello World"
         }
     chan.assertQueue(queue, {
-        durable : false
+        durable : true 
     })
-    let res = chan.sendToQueue(queue,Buffer.from(JSON.stringify(msg))) 
+    let res = chan.sendToQueue(queue,Buffer.from(JSON.stringify(msg)),{
+        persistent : true  
+    }) 
     
-    console.log(`send ok ? ${res}`)
+    console.log(`send ok ? ${res} ${JSON.stringify(msg)}`)
     
     setTimeout( async ()=>{
         await con.close()
